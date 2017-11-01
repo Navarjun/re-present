6971ad10821f9da6ed2a11fd3665bdcefdb3caf7
iMac <imac@iMacs-iMac.local>
2017-11-01 14:52:15 -0400

    Project setup with React/Webpack

---
+++ app/src/App.jsx
@@ -0,0 +1,33 @@
+import React, {Component} from 'react'
+import {render} from 'react-dom'
+import {} from './styles/global.css'
+import Logo from './components/Logo.jsx'
+import Link from './components/Link.jsx'
+
+import {Box} from 'react-desktop'
+
+const logos = [
+    require('./assets/electron.png'),
+    require('./assets/react.png'),
+    require('./assets/webpack.png')
+]
+
+
+export default class App extends Component {
+    render() {
+        const logosRender = logos.map( (logo, index) => {
+            return <Logo key = {index} src = { logo } />
+        })
+
+        return (
+            <div id="app">
+            <div style={{background: 'lightgrey'}}>
+            </div>
+            <div style={{background: 'seagreen'}}>
+            </div>
+            <div style={{background: 'steelblue'}}>
+            </div>
+            </div>
+        )
+    }
+}
